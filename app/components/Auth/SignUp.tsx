24539d6b0a16c84de36b16f900fee92ccf41b1c3
chintamani-pala <chintamanipala67@gmail.com>
2024-08-17 07:20:43 +0530

    design of login page and sign up page is updated

--- app/components/Auth/SignUp.tsx
+++ app/components/Auth/SignUp.tsx
@@ -116,9 +116,9 @@ const SignUp: FC<Props> = ({ setRoute }) => {
         </h5>
         <div className="flex items-center justify-center my-3">
           <FcGoogle size={30} className="cursor-pointer mr-2" />
-          <AiFillGithub size={30} className="cursor-pointer ml-2" />
+          <AiFillGithub size={30} className="cursor-pointer ml-2 text-black dark:text-white" />
         </div>
-        <h5 className="text-center pt-4 font-Poppins text-[14px]">
+        <h5 className="text-center pt-4 font-Poppins text-[14px] text-black dark:text-white">
           Already have account?
           <span
             className="text-[#2190ff] pl-1 cursor-pointer"
