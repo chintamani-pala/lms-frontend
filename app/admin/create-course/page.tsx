06606770ffffde3f33636a6f24abe6818f509612
CHINTAMANI PALA <chintamanipala67@gmail.com>
2024-10-26 22:55:53 +0530

    user delete , course delet

--- app/admin/create-course/page.tsx
+++ app/admin/create-course/page.tsx
@@ -4,26 +4,29 @@ import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
 import Heading from "../../../app/utils/Heading";
 import CreateCourse from "../../components/Admin/Course/CreateCourse";
 import DashboardHeader from "../../../app/components/Admin/DashboardHeader";
+import AdminProtected from "@/app/hooks/adminProtected";
 
 type Props = {};
 
 const page = (props: Props) => {
   return (
     <div>
-      <Heading
-        title="Create Course"
-        description="Learning Management System"
-        keywords="Programming,LMS,Learning,Computer Scienc,Web Development"
-      />
-      <div className="flex">
-        <div className="1500px:w-[16%] w-1/5">
-          <AdminSidebar />
+      <AdminProtected>
+        <Heading
+          title="Create Course"
+          description="Learning Management System"
+          keywords="Programming,LMS,Learning,Computer Scienc,Web Development"
+        />
+        <div className="flex">
+          <div className="1500px:w-[16%] w-1/5">
+            <AdminSidebar />
+          </div>
+          <div className="w-[85%]">
+            <DashboardHeader />
+            <CreateCourse />
+          </div>
         </div>
-        <div className="w-[85%]">
-          <DashboardHeader />
-          <CreateCourse />
-        </div>
-      </div>
+      </AdminProtected>
     </div>
   );
 };
